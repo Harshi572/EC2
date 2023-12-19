159061d08590d82b63acad7ff09e290bef6ed2de
Harshitha Kollipaka <85920657+Harshi572@users.noreply.github.com>
2023-12-19 12:42:51 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,9 +1,4 @@
-# Get input from the user
 n1 = int(input("Enter the first number: "))
 n2 = int(input("Enter the second number: "))
-
-# Calculate the sum
-sum_result = n1 + n2
-
-# Print the result
-print(sum_result)
+sum= n1 + n2
+print(sum)
