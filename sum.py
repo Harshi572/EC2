8203ab3bd6b0fd784b8009975c451b1d7865d9e1
Harshitha Kollipaka <85920657+Harshi572@users.noreply.github.com>
2023-12-19 10:49:51 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,9 @@
+# Get input from the user
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+
+# Calculate the sum
+sum_result = n1 + n2
+
+# Print the result
+print(sum_result)
